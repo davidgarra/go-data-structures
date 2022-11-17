ab46cfebce499178f148620065488be4e4fad359
David Garra <david.garra@ext.grupposandonato.it>
2022-11-17 19:35:36 +0100

    handle map collision

--- map.go
+++ map.go
@@ -1,16 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Bucket[K string, V any] struct {
 	key   K
 	value V
 }
 
 type Map[K string, V any] struct {
-	Data []Bucket[K, V]
+	Data [][]Bucket[K, V]
 }
 
 func NewMap[K string, V any](size int) Map[K, V] {
-	return Map[K, V]{Data: make([]Bucket[K, V], size)}
+	return Map[K, V]{Data: make([][]Bucket[K, V], size)}
 }
 
 func (m *Map[K, V]) hash(key K) uint {
@@ -23,12 +27,29 @@ func (m *Map[K, V]) hash(key K) uint {
 	return hash
 }
 
-func (m *Map[K, V]) Set(key K, value V) {
-	hk := m.hash(key)
-	m.Data[hk] = Bucket[K, V]{key, value}
+func findBucket[K string, V any](bl []Bucket[K, V], key K) (Bucket[K, V], bool) {
+	for _, b := range bl {
+		if b.key == key {
+			return b, true
+		}
+	}
+
+	return Bucket[K, V]{}, false
 }
 
-func (m *Map[K, V]) Get(key K) V {
+func (m *Map[K, V]) Set(key K, value V) {
 	hk := m.hash(key)
-	return m.Data[hk].value
+	bucketList := m.Data[hk]
+	if _, found := findBucket(bucketList, key); found {
+		fmt.Printf("Key %v already exists\n", key)
+		return
+	}
+	bucketList = append(bucketList, Bucket[K, V]{key, value})
+	m.Data[hk] = bucketList
+}
+
+func (m *Map[K, V]) Get(key K) (V, bool) {
+	hk := m.hash(key)
+	bucket, found := findBucket(m.Data[hk], key)
+	return bucket.value, found
 }
