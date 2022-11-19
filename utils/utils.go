bb829b6a4ba46fde8d107ad9d1507af0f84660a2
David Garra <david.garra@ext.grupposandonato.it>
2022-11-19 17:16:50 +0100

    add utils package

---
+++ utils/utils.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"log"
+	"os"
+)
+
+func Zero[T any]() (ret T) {
+	return
+}
+
+func Quiet() func() {
+	null, _ := os.Open(os.DevNull)
+	sout := os.Stdout
+	serr := os.Stderr
+	os.Stdout = null
+	os.Stderr = null
+	log.SetOutput(null)
+	return func() {
+		defer null.Close()
+		os.Stdout = sout
+		os.Stderr = serr
+		log.SetOutput(os.Stderr)
+	}
+}
